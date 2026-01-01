93c75674f439bdd05990ecb7053eb3d10cef3668
shyleshwaran <shyleshw@gmail.com>
2026-01-01 22:08:59 +0530

    Enhance city handling in weather app
    Refactor city existence check to be case-insensitive and improve deletion feedback.

--- the_weather/weatherapp/views.py
+++ the_weather/weatherapp/views.py
@@ -7,12 +7,13 @@ from django.contrib import messages
 def home(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=e8d7d6f4fe92b6dd78e2415a5c765d00&units=metric'
 
-    if request.method == "POST":
+   if request.method == "POST":
         form = CityForm(request.POST)
 
         if form.is_valid():
             NCity = form.cleaned_data['name']
-            city_exists = City.objects.filter(name=NCity).count()
+            city_exists = City.objects.filter(name__iexact=NCity).exists()
+
 
             if city_exists == 0:
                 res = requests.get(url.format(NCity))
@@ -49,6 +50,12 @@ def home(request):
 
 
 def delete_city(request, CName):
-    City.objects.get(name=CName).delete()
-    messages.success(request, CName + " Removed Successfully!")
+    deleted, _ = City.objects.filter(name__iexact=CName).delete()
+
+    if deleted:
+        messages.success(request, f"{CName} removed successfully!")
+    else:
+        messages.error(request, f"{CName} not found!")
+
     return redirect('home')
+
